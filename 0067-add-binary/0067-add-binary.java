b1d38ea98e179fc93689f8d656ace173264eab2b
Shanu Kumar <beingshanu789@gmail.com>
2026-06-30 18:12:32 +0530

    Time: 2 ms (47.97%), Space: 43.4 MB (69.39%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,78 +1,29 @@
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder ans = new StringBuilder();
-        char carry = '0';
-        int i = a.length()-1;
-        int j = b.length()-1;
-        while(i>=0 && j>=0){
-            if(carry == '0'){
-                if(a.charAt(i)=='1' && b.charAt(j)=='1'){
-                    ans.append('0');
-                    carry = '1';
-                }else if ((a.charAt(i)=='0' && b.charAt(j)=='1') || (a.charAt(i)=='1' && b.charAt(j)=='0')){
-                    ans.append(1);
-                }else if((a.charAt(i)=='0' && b.charAt(j)=='0') || (a.charAt(i)=='0' && b.charAt(j)=='0')){
-                    ans.append('0');
-                }
-            }else {
-                if(a.charAt(i)=='1' && b.charAt(j)=='1'){
-                    ans.append('1');
-                    // carry = '1';
-                }else if ((a.charAt(i)=='0' && b.charAt(j)=='1') || (a.charAt(i)=='1' && b.charAt(j)=='0')){
-                    ans.append('0');
-                    // carry = '1';
-                }else if((a.charAt(i)=='0' && b.charAt(j)=='0') || (a.charAt(i)=='0' && b.charAt(j)=='0')){
-                    ans.append('1');
-                    carry = '0';
-                }
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
             }
-            i--;
-            j--;
-        }
-        while(i>=0){
-            if(carry == '0'){
-                if(a.charAt(i) == '1'){
-                    ans.append('1');
-                    carry = '0';
-                }else {
-                    ans.append('0');
-                    carry = '0';
-                }
-            }else {
-                if(a.charAt(i) == '1'){
-                    ans.append('0');
-                    carry = '1';
-                }else {
-                    ans.append('1');
-                    carry = '0';
-                }
+
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
             }
-            i--;
+
+            ans.append(sum % 2);
+            carry = sum / 2;
         }
-        while(j>=0){
-            if(carry == '0'){
-                if(b.charAt(j) == '1'){
-                    ans.append('1');
-                    carry = '0';
-                }else {
-                    ans.append('0');
-                    carry = '0';
-                }
-            }else {
-                if(b.charAt(j) == '1'){
-                    ans.append('0');
-                    carry = '1';
-                }else {
-                    ans.append('1');
-                    carry = '0';
-                }
-            }
-            j--;
-        }
-        if(carry == '1'){
-            ans.append(carry);
-        }
-        ans.reverse();
-        return ans.toString();
+
+        return ans.reverse().toString();
     }
 }
