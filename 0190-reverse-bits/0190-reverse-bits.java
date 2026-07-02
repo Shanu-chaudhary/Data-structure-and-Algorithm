c12d2d10cdcccbeef1a71902a3adba9f85f81c54
Shanu Kumar <beingshanu789@gmail.com>
2026-07-02 18:30:58 +0530

    Time: 2 ms (9.43%), Space: 42.4 MB (52.16%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int reverseBits(int n) {
+        // StringBuilder sb = new StringBuilder();
+        // while(n>0){
+        //     int rem = n % 2;
+        //     sb.append(rem);
+        //     n = n/2;
+        // }
+        // sb.reverse();
+        StringBuilder sb = new StringBuilder();
+
+for (int i = 0; i < 32; i++) {
+    sb.append(n & 1);
+    n >>>= 1;
+}
+        int ans = 0;
+        int p2 = 1;
+        for(int i=sb.length()-1; i>=0; i--){
+            char ch = sb.charAt(i);
+            int num = ch - '0';
+            ans = ans + (p2 * num);
+            p2 = p2 * 2;
+        }
+        return ans;
+    }
+}
