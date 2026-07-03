e7893e0dd9b6962a91d9d4294e78cfc767dac93e
Shanu Kumar <beingshanu789@gmail.com>
2026-07-03 19:30:01 +0530

    Time: 10 ms (7.25%), Space: 45.7 MB (34.36%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,11 +1,23 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ones = 0;
-        int twos = 0;
-        for(int i=0; i<nums.length; i++){
-            ones = ((ones^nums[i]) & (~twos));
-            twos = ((twos^nums[i]) & (~ones));
+
+        // most optimised
+
+        // int ones = 0;
+        // int twos = 0;
+        // for(int i=0; i<nums.length; i++){
+        //     ones = ((ones^nums[i]) & (~twos));
+        //     twos = ((twos^nums[i]) & (~ones));
+        // }
+        // return ones;
+
+        // nlogn + n
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i+=3){
+            if(nums[i] != nums[i-1]){
+                return nums[i-1];
+            }
         }
-        return ones;
+        return nums[nums.length-1];
     }
 }
