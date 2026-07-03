c02359a1b9bf56caea61a61fe4a8a2f8fc545355
Shanu Kumar <beingshanu789@gmail.com>
2026-07-03 18:45:23 +0530

    Time: 1 ms (61.46%), Space: 42.3 MB (69.38%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,5 +1,13 @@
 class Solution {
     public int reverseBits(int n) {
+        int ans = 0;
+        for(int i=0; i<32; i++){
+            ans = ans << 1;
+            ans = ans | (n&1);
+            n>>>=1;   // unsigned right shift operator for -ve numbers
+        }
+        return ans;
+
         // StringBuilder sb = new StringBuilder();
         // while(n>0){
         //     int rem = n % 2;
@@ -7,20 +15,20 @@ class Solution {
         //     n = n/2;
         // }
         // sb.reverse();
-        StringBuilder sb = new StringBuilder();
+//         StringBuilder sb = new StringBuilder();
 
-for (int i = 0; i < 32; i++) {
-    sb.append(n & 1);
-    n >>>= 1;
-}
-        int ans = 0;
-        int p2 = 1;
-        for(int i=sb.length()-1; i>=0; i--){
-            char ch = sb.charAt(i);
-            int num = ch - '0';
-            ans = ans + (p2 * num);
-            p2 = p2 * 2;
-        }
-        return ans;
+// for (int i = 0; i < 32; i++) {
+//     sb.append(n & 1);
+//     n >>>= 1;
+// }
+//         int ans = 0;
+//         int p2 = 1;
+//         for(int i=sb.length()-1; i>=0; i--){
+//             char ch = sb.charAt(i);
+//             int num = ch - '0';
+//             ans = ans + (p2 * num);
+//             p2 = p2 * 2;
+//         }
+//         return ans;
     }
 }
