4071e020531431c4ed25e00b39c78bc8e09b6aa3
Shanu Kumar <beingshanu789@gmail.com>
2026-07-27 16:56:31 +0530

    Time: 13 ms (80.15%), Space: 50.9 MB (5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,3 +1,11 @@
+// class Pair {
+//     int num;
+//     int freq;
+//     Pair(int num, int freq){
+//         this.num = num;
+//         this.freq = freq;
+//     }
+// }
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         int n = nums.length;
@@ -5,13 +13,32 @@ class Solution {
         for(int i=0; i<n; i++){
             mp.put(nums[i], mp.getOrDefault(nums[i], 0)+1);
         }
-        ArrayList<Map.Entry<Integer,Integer>> arr =
-                new ArrayList<>(mp.entrySet());
-        arr.sort((a,b)->b.getValue()-a.getValue());
-        int[] last = new int[k];
-        for(int i=0;i<k;i++){
-            last[i]=arr.get(i).getKey();
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        for(int i=0; i<n+1; i++){
+            arr.add(new ArrayList<>());
         }
-        return last;
+        for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
+            int idx = entry.getValue();
+            arr.get(idx).add(entry.getKey());
+        }
+        ArrayList<Integer> fans = new ArrayList<>();
+        for(int i=arr.size()-1; i>=0; i--){
+            if(arr.get(i).size() == 0){
+                continue;
+            }
+            int l = arr.get(i).size();
+            while(l > 0 && k > 0){
+                fans.add(arr.get(i).getLast());
+                arr.get(i).remove(l-1);
+                l--;
+                k--;
+            }
+        }
+        int[] ans = new int[fans.size()];
+        int l = ans.length;
+        for(int j=0; j<l; j++){
+            ans[j] = fans.get(j);
+        }
+        return ans;
     }
 }
