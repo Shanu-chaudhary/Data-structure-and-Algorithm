fa19bc2ffe2adac12d747fe38ad014fffc9c8244
Shanu Kumar <beingshanu789@gmail.com>
2026-07-31 13:03:31 +0530

    Time: 9 ms (94.22%), Space: 48.3 MB (17.34%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,27 +1,23 @@
 class Solution {
     public int minimumPushes(String word) {
         int n = word.length();
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] arr = new int[26];
         for(int i=0; i<n; i++){
             char ch = word.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0)+1);
+            arr[ch - 'a']++;
         }
-        // sort hashMap in desc order by their freq
-        List<Map.Entry<Character, Integer>> entryList = new ArrayList<>(map.entrySet());
-        entryList.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
-        Map<Character, Integer> sortedMap = new LinkedHashMap<>();
-        for (Map.Entry<Character, Integer> entry : entryList) {
-            sortedMap.put(entry.getKey(), entry.getValue());
+        Arrays.sort(arr);
+        int[] freq = new int[26];
+        for(int i=0; i<26; i++){
+            freq[i] = arr[25-i];
         }
         int ans = 0;
-        int idx = 0;
-        for(Map.Entry<Character, Integer> entry : sortedMap.entrySet()){
-            char ch = entry.getKey();
-            int freq = entry.getValue();
-            ans += ((idx/8)+1)*freq;
-            idx++;
+        for(int i=0; i<26; i++){
+            if(freq[i] == 0){
+                break;
+            }
+            ans += ((i/8)+1)*freq[i];
         }
         return ans;
-
     }
 }
